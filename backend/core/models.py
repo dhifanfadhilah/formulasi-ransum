5404023044934491a1d6458b8c6ce59fa4f30b8c
Dhifan <dhifanfadhilahaditya@gmail.com>
2025-05-19 17:40:08 +0700

    add model database

--- backend/core/models.py
+++ backend/core/models.py
@@ -35,3 +35,99 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+# Jenis & Fase Unggas
+class JenisUnggas(models.Model):
+    nama = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nama
+
+
+class FaseUnggas(models.Model):
+    nama = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nama
+
+# Bahan Pakan
+class BahanPakan(models.Model):
+    nama = models.CharField(max_length=100)
+    harga = models.DecimalField(max_digits=10, decimal_places=2)
+    kadar_air = models.DecimalField(max_digits=5, decimal_places=2)
+    bahan_kering = models.DecimalField(max_digits=5, decimal_places=2)
+    abu = models.DecimalField(max_digits=5, decimal_places=2)
+    pk = models.DecimalField(max_digits=5, decimal_places=2)
+    lk = models.DecimalField(max_digits=5, decimal_places=2)
+    sk = models.DecimalField(max_digits=5, decimal_places=2)
+    betn = models.DecimalField(max_digits=5, decimal_places=2)
+    me = models.DecimalField(max_digits=10, decimal_places=2)
+    ca = models.DecimalField(max_digits=5, decimal_places=2)
+    p_total = models.DecimalField(max_digits=5, decimal_places=2)
+    p_tersedia = models.DecimalField(max_digits=5, decimal_places=2)
+    lys = models.DecimalField(max_digits=5, decimal_places=2)
+    met = models.DecimalField(max_digits=5, decimal_places=2)
+    met_sis = models.DecimalField(max_digits=5, decimal_places=2)
+    tre = models.DecimalField(max_digits=5, decimal_places=2)
+    trypt = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nama
+    
+# Kebutuhan Nutrien
+class KebutuhanNutrien(models.Model):
+    jenis_unggas = models.ForeignKey(JenisUnggas, on_delete=models.CASCADE)
+    fase = models.ForeignKey(FaseUnggas, on_delete=models.CASCADE)
+
+    bk_min = models.DecimalField(max_digits=5, decimal_places=2)
+    bk_max = models.DecimalField(max_digits=5, decimal_places=2)
+    abu_min = models.DecimalField(max_digits=5, decimal_places=2)
+    abu_max = models.DecimalField(max_digits=5, decimal_places=2)
+    pk_min = models.DecimalField(max_digits=5, decimal_places=2)
+    pk_max = models.DecimalField(max_digits=5, decimal_places=2)
+    lk_min = models.DecimalField(max_digits=5, decimal_places=2)
+    lk_max = models.DecimalField(max_digits=5, decimal_places=2)
+    sk_min = models.DecimalField(max_digits=5, decimal_places=2)
+    sk_max = models.DecimalField(max_digits=5, decimal_places=2)
+    betn_min = models.DecimalField(max_digits=5, decimal_places=2)
+    betn_max = models.DecimalField(max_digits=5, decimal_places=2)
+    me_min = models.DecimalField(max_digits=10, decimal_places=2)
+    me_max = models.DecimalField(max_digits=10, decimal_places=2)
+    ca_min = models.DecimalField(max_digits=5, decimal_places=2)
+    ca_max = models.DecimalField(max_digits=5, decimal_places=2)
+    p_total_min = models.DecimalField(max_digits=5, decimal_places=2)
+    p_total_max = models.DecimalField(max_digits=5, decimal_places=2)
+    p_tersedia_min = models.DecimalField(max_digits=5, decimal_places=2)
+    p_tersedia_max = models.DecimalField(max_digits=5, decimal_places=2)
+    lys_min = models.DecimalField(max_digits=5, decimal_places=2)
+    lys_max = models.DecimalField(max_digits=5, decimal_places=2)
+    met_min = models.DecimalField(max_digits=5, decimal_places=2)
+    met_max = models.DecimalField(max_digits=5, decimal_places=2)
+    met_sis_min = models.DecimalField(max_digits=5, decimal_places=2)
+    met_sis_max = models.DecimalField(max_digits=5, decimal_places=2)
+    tre_min = models.DecimalField(max_digits=5, decimal_places=2)
+    tre_max = models.DecimalField(max_digits=5, decimal_places=2)
+    trypt_min = models.DecimalField(max_digits=5, decimal_places=2)
+    trypt_max = models.DecimalField(max_digits=5, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# Formulasi & Bahan Formulasi
+class Formulasi(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    unggas = models.ForeignKey(JenisUnggas, on_delete=models.CASCADE)
+    fase = models.ForeignKey(FaseUnggas, on_delete=models.CASCADE)
+    formulasi = models.JSONField()
+    total_harga = models.DecimalField(max_digits=12, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class BahanFormulasi(models.Model):
+    formulasi = models.ForeignKey(Formulasi, on_delete=models.CASCADE, related_name='bahan_formulasi')
+    bahan_pakan = models.ForeignKey(BahanPakan, on_delete=models.CASCADE)
+    jumlah = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.bahan_pakan.nama} - {self.jumlah} kg"
